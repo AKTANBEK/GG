e42d0d2bbb592b90d6cfb56995c37ce12ff16543
AKTANBEK <uaqwe1147@gmail.com>
2021-05-03 20:56:35 +0600

    first letter

--- test.py
+++ test.py
@@ -4,3 +4,9 @@ print('eeafeafe')
 print(1+1)
 print(2+2)
 print('forms')
+
+
+lst = ['Nurkyz', 'Adilet', 'Abai', 'Siymyk', 'Ulan', 'Adilet', 'Richard', 'Insan', 'Azim']
+
+for i in lst:
+    print(i[0])
